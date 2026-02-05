aed7fa487d6c615d3aa1fd787a1ebda2493deabc
wqLouis <wongqiluo@gmail.com>
2026-02-06 00:33:08 +0800

    fix(main): fix progressbar never finish bug

--- src/main.rs
+++ src/main.rs
@@ -46,7 +46,9 @@ fn main() {
                     String::from_utf8_lossy(payload).to_string(),
                 );
             }
-            _ => {}
+            _ => {
+                pb.inc(1);
+            }
         }
     }
 
@@ -54,7 +56,6 @@ fn main() {
         handle.join().unwrap();
         pb.inc(1);
     }
-    pb.finish_and_clear();
 
     let scene: scene::Root = serde_json::from_str(jsons.get("scene.json").unwrap()).unwrap();
 
