901baaaabdef2b07e0b88ea562d437a178191283
wqLouis <wongqiluo@gmail.com>
2026-02-07 01:17:52 +0800

    refactor(render): change background color to black
    So it wont blind you

--- src/scene/render.rs
+++ src/scene/render.rs
@@ -103,7 +103,7 @@ impl WgpuApp {
                     depth_slice: None,
                     ops: Operations {
                         load: LoadOp::Clear(Color {
-                            r: 1.0,
+                            r: 0.0,
                             g: 0.0,
                             b: 0.0,
                             a: 1.0,
